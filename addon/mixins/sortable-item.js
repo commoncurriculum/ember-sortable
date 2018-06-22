840e0907314bd05394769215aa779f628b6d3c4a
Scott Ames-Messinger <scott@commoncurriculum.com>
2018-06-22 18:26:14 -0400

    Remove reference to ember-new-computed

--- addon/mixins/sortable-item.js
+++ addon/mixins/sortable-item.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import computed from 'ember-new-computed';
+import { computed } from '@ember/object'; 
 const { Mixin, $, run } = Ember;
 const { Promise } = Ember.RSVP;
 
